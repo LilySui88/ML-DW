c47641c9cb07ba378345f8e7d1547fa5377f268b
隋英丽 <suiyingli88@gmail.com>
2016-03-24 19:39:45 +0800

    kNN test

--- kNN.py
+++ kNN.py
@@ -1,5 +1,9 @@
 from numpy import *
 import operator
+from os import listdir
+import matplotlib
+import matplotlib.pyplot as plt
+
 def createDataSet():
     group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
     labels = ['A','A','B','B']
@@ -31,3 +35,84 @@ def file2matrix(filename):
 		classLabelVector.append(int(listFromLine[-1]))
 		index+=1
 	return returnMat,classLabelVector
+def autoNorm(dataSet):
+	minVals=dataSet.min(0)
+	maxVals=dataSet.max(0)
+	ranges=maxVals-minVals
+	normDataSet=zeros(shape(dataSet))
+	m=dataSet.shape[0]
+	normDataSet=dataSet-tile(minVals,(m,1))
+	normDataSet=normDataSet/tile(ranges,(m,1))
+	return normDataSet,ranges,minVals
+def datingClassTest():
+	hoRatio=0.10
+	datingDataMat,datingLabels=file2matrix('datingTestSet2.txt')
+	normMat,ranges,minVals=autoNorm(datingDataMat)
+	m=normMat.shape[0]
+	numTestVecs=int(m*hoRatio)
+	errorCount=0.0
+	for i in range(numTestVecs):
+		classifierResult=classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+		print "the classifier came back with:%d,the real answer is:%d"%(classifierResult,datingLabels[i])
+		if(classifierResult!=datingLabels[i]):
+			errorCount+=1.0
+			print " +"
+	print "the total error rate is:%f"%(errorCount/float(numTestVecs)) 
+def classifyPerson():
+	resultList=['not at all','in small doses','in large doses']
+	precentTats=float(raw_input("percentage of time spent playing video games?"))
+	ffMiles=float(raw_input("frequent flier miles earned per year?"))
+	iceCream=float(raw_input("liters of ice cream consumed per year?"))
+	datingDataMat,datingLabels=file2matrix('datingTestSet2.txt')
+	normMat,ranges,minVals=autoNorm(datingDataMat)
+	inArr=array([ffMiles,precentTats,iceCream])
+	classifierResult=classify0((inArr-minVals)/ranges,normMat,datingLabels,3)
+	print "You jwill probably like this person: ",resultList[classifierResult-1]
+def img2vector(filename):
+	returnVect=zeros((1,1024))
+	fr=open(filename)
+	for i in range(32):
+		lineStr=fr.readline()
+		for j in range(32):
+			returnVect[0,32*i+j]=int(lineStr[j])
+	return returnVect
+def handwringClassTesst():
+	hwLabels=[]
+	trainingFileList=listdir('trainingDigits')
+	m=len(trainingFileList)
+	trainingMat=zeros((m,1024))
+	for i in range(m):
+		fileNameStr=trainingFileList[i]
+		fileStr=fileNameStr.split('.')[0]
+		classNumStr=int(fileNameStr.split('_')[0])
+		hwLabels.append(classNumStr)
+		trainingMat[i,:]=img2vector('trainingDigits/%s'%fileNameStr)
+	testFileList=listdir('testDigits')
+	errorCount=0.0
+	mTest=len(testFileList)
+	for i in range(mTest):
+		fileNameStr=testFileList[i]
+		fileStr=fileNameStr.split('.')[0]
+		classNumStr=int(fileNameStr.split('_')[0])
+		vectorUnderTest=img2vector('testDigits/%s'%fileNameStr)
+		classifierResult=classify0(vectorUnderTest,trainingMat,hwLabels,3)
+		print "the classifier came back with:%d,the real answer is:%d"%(classifierResult,classNumStr)
+		if(classifierResult!=classNumStr):
+			errorCount+=1.0
+			print " +"
+	print "\n total number of errors is: %d"%errorCount
+	print "\n total error rate is: %s" %(errorCount/float(mTest))
+def testPlot():
+	#test classify0
+	group,labels=createDataSet()
+	result=classify0([0,0],group,labels,3)
+	#test file2matrix
+	datingDataMat,datingDataLabels=file2matrix('datingTestSet2.txt')
+	#test pyplot
+	fig=plt.figure()
+	ax=fig.add_subplot(111)
+	#no several colors
+	#ax.scatter(datingDataMat[:,1],datingDataMat[:,2])
+	#two colors
+	ax.scatter(datingDataMat[:,1],datingDataMat[:,2],15.0*array(datingDataLabels),15.0*array(datingDataLabels))
+	plt.show()
