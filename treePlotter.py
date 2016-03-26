4f10a6a91c7ce62d8f8cf216eabb9f3085dc72b6
隋英丽 <suiyingli88@gmail.com>
2016-03-26 18:59:42 +0800

    treeplot

--- treePlotter.py
+++ treePlotter.py
@@ -15,3 +15,24 @@ def createPlot():
 	plotNode(U'决策节点',(0.5,0.1),(0.1,0.5),decisionNode)
 	plotNode(U'叶节点',(0.8,0.1),(0.3,0.8),leafNode)
 	plt.show()
+def getNumLeafs(myTree):
+	numLeafs=0
+	firstStr=myTree.keys()[0]
+	secondDict=myTree[firstStr]
+	for key in secondDict.keys():
+		if type(secondDict[key]).__name__=='dict':
+			numLeafs+=getNumLeafs(secondDict[key])
+		else:
+			numLeafs+=1
+	return numLeafs
+def getTreeDepth(myTree):
+	maxDepth=0
+	firstStr=myTree.keys()[0]
+	secondDict=myTree[firstStr]
+	for key in secondDict.keys():
+		if type(secondDict[key]).__name__=='dict' :
+			thisDepth=1+getTreeDepth(secondDict[key])
+		else:
+			thisDepth=1
+		if thisDepth>maxDepth:maxDepth=thisDepth
+	return maxDepth
